ea60c8796be7b14a6abb5899b7c1821f120dfcfb
Annur Afgoni <afgoniannur@gmail.com>
2024-01-27 19:31:00 +0800

    update predict.py

--- classification_model/predict.py
+++ classification_model/predict.py
@@ -1,16 +1,18 @@
 import numpy as np
 import pandas as pd
 
-from classification_model.processing.data_management import load_pipeline
-from classification_model.config import  config
+from processing.data_management import load_pipeline
+from config.core import config
 
-pipeline_file_name = "titanic_classification_model.pkl"
+from classification_model import __version__ as _version
+
+pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
 _titanic_pipe = load_pipeline(file_name=pipeline_file_name)
 
 def make_prediction(*, input_data) -> dict:
 
     data = pd.read_json(input_data)
-    prediction = _titanic_pipe.predict(data[config.FEATURES])
+    prediction = _titanic_pipe.predict(data[config.app_config.features])
     response = {"prediction": prediction}
 
     return response
