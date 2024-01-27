5f130b7226c275f508a706f865d6fc5815c93936
Annur Afgoni <afgoniannur@gmail.com>
2024-01-27 19:25:04 +0800

    update train_pipeline.py

--- classification_model/train_pipeline.py
+++ classification_model/train_pipeline.py
@@ -1,27 +1,27 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-from classification_model import pipeline
-from classification_model.config import config
-from classification_model.processing.data_management import (
+from pipeline import titanic_pipe
+from config.core import config
+from processing.data_management import (
     load_dataset,
     save_pipeline
 )
 
 def run_training() -> None:
 
-    data = load_dataset(file_name=config.TRAINING_DATA_FILE)
+    data = load_dataset(file_name=config.app_config.training_data_file)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        data[config.FEATURES],
-        data[config.TARGET],
-        test_size=0.1,
-        random_state=0
+        data[config.model_config.features],
+        data[config.model_config.target],
+        test_size=config.model_config.test_size,
+        random_state=config.model_config.random_state
     )
 
-    pipeline.titanic_pipe.fit(X_train[config.FEATURES], y_train)
+    titanic_pipe.fit(X_train[config.model_config.features], y_train)
 
-    save_pipeline(pipeline_to_persist=pipeline.titanic_pipe)
+    save_pipeline(pipeline_to_persist=titanic_pipe)
 
 if __name__ == "__main__":
     run_training()
